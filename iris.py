4d35da297abb2dad4b84e307f702ab7dcceb1f02
aburioz1 <97783316+aburioz1@users.noreply.github.com>
2023-10-18 21:47:35 +0100

    Update iris.py

--- iris.py
+++ iris.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 import pickle
-iris_model = pickle.load(open("C:\\Users\\HP\\Downloads\\iris_model.pkl", "rb"))
+iris_model = pickle.load(open("iris_model.pkl", "rb"))
 st.title("Iris Flower Prediction App")
-image = Image.open('C:\\Users\\HP\\Downloads\\pexels-elina-nova-13797008.jpg')
+image = Image.open('pexels-elina-nova-13797008.jpg')
 st.image(image, width= 300)
 def user_report():
     sepal_length = st.sidebar.slider('sepal length',1.0,10.0,0.1)
@@ -27,15 +27,15 @@ prediction = iris_model.predict(user_data)
 st.success(prediction)
 if (prediction ==0):
     st.success('setosa')
-    image = Image.open('C:\\Users\\HP\\Downloads\\Irissetosa1.jpg')
+    image = Image.open('Irissetosa1.jpg')
     st.image(image, width=300)
 elif (prediction ==1):
     st.success('versicolor')
-    image = Image.open('C:\\Users\\HP\\Downloads\\Blue_Flag,_Ottawa.jpg')
+    image = Image.open('Blue_Flag,_Ottawa.jpg')
     st.image(image, width=300)
 else:
     st.success('virginica')
-    image = Image.open('C:\\Users\\HP\\Downloads\\Iris_virginica_2.jpg')
+    image = Image.open('Iris_virginica_2.jpg')
     st.image(image, width=300)
 
 
